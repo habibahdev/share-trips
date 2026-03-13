8cc4be9fc07530021c6c1228aca71e529bf55710
habibahdev <ummhabibah-ouattara@outlook.com>
2026-03-13 13:35:58 +0100

    feat: register, login

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,25 +4,49 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email'
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+                    'attr'  => ['autocomplete' => 'new-password'],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez le mot de passe',
+                    'attr'  => ['autocomplete' => 'new-password'],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'constraints' => [
+                    new Length(min: 6, minMessage: 'Le mot de passe doit contenir au moins {{ limit }} caractères.'),
+                    new NotBlank(message: 'Le mot de passe est obligatoire.')
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => false
             ])
         ;
     }
